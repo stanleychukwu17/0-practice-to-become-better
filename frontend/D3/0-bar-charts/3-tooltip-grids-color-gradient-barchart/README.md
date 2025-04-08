edbba18d89968c59340b156e7aa2e9419de4a326
Chukwu Stanley <stanleychukwu17@yahoo.com>
2025-04-08 16:29:43 +0100

    initial commit

---
+++ frontend/D3/0-bar-charts/3-tooltip-grids-color-gradient-barchart/README.md
@@ -0,0 +1,27 @@
+# Simple BarChart with D3
+
+1. start the server: pnpm/npm dev
+
+2. the code is located in the src/components/BarChart/BarChart1.tsx
+    - we want a similar result to BarChart.tsx
+    - you can read note on animation: [note](./note.txt)
+    - carry on with your animating the bars
+
+3. test that everything works fine
+    - commit all changes made to github
+
+5. clean up
+    - take the code in BarChart1.tsx back to Default.tsx
+    - delete App1.tsx, BarChart1.tsx, node_modules
+    - commit all changes after clean up
+
+<br>
+<br>
+
+# Final result should look like:
+hover over the video link to watch the video
+
+<video width="640" height="360" controls>
+  <source src="barChart-animated.mp4" type="video/mp4">
+  Your browser does not support the video tag.
+</video>
---
+++ frontend/D3/0-bar-charts/3-tooltip-grids-color-gradient-barchart/src/components/BarChart/BarChart.tsx
@@ -0,0 +1,85 @@
+import { useRef, useEffect } from "react";
+import * as d3 from "d3";
+
+type DataPoint = {
+  name: string;
+  value: number;
+};
+
+type BarChartProps = {
+  data: DataPoint[];
+  width?: number;
+  height?: number;
+};
+
+export default function BarChart({data, width = 500, height = 300} : BarChartProps) {
+  const svgRef = useRef<SVGSVGElement>(null)
+
+  useEffect(() => {
+    if (!svgRef.current) return
+
+    const svg = d3.select(svgRef.current);
+    svg.selectAll("*").remove(); // clear previous renders
+
+    const margin = { top: 20, right: 20, bottom: 50, left: 50 };
+    const chartWidth = width - margin.left - margin.right;
+    const chartHeight = height - margin.top - margin.bottom;
+
+    // Scales
+    const xScale = d3
+      .scaleBand<string>()
+      .domain(data.map((d) => d.name)) // [A, B, C, D, E]
+      .range([0, chartWidth])
+      .padding(0.1);
+
+    const yScale = d3
+      .scaleLinear()
+      .domain([0, d3.max(data, (d) => d.value) as number]) // you can also do: .domain([0, d3.max(data, (d) => d.value) || 0])
+      .nice()
+      .range([chartHeight, 0]);
+
+    // create a group element for the charts
+    const chart = svg
+      .append("g")
+      .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    // Bars
+    chart
+      .selectAll("rect")
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("x", (d) => xScale(d.name) as unknown as string )
+      .attr("y", chartHeight)
+      .attr("width", xScale.bandwidth())
+      .attr("height", 0)
+      .attr("fill", "pink")
+      .transition()
+      .duration(800)
+      .delay((_, i) => i * 300) // where _ = d, but since we're not using it, we replace it with _
+      .attr("y", (d) => yScale(d.value) )
+      .attr("height", (d) => chartHeight - yScale(d.value))
+
+    // X Axis
+    chart
+      .append("g")
+      .attr("transform", `translate(0, ${chartHeight})`)
+      .call(d3.axisBottom(xScale))
+      .attr("color", "black");
+
+    // Y Axis
+    chart.append("g").call(d3.axisLeft(yScale)).attr("color", "black");
+
+  }, [data, height, width])
+
+  return (
+    <div style={{ minWidth:"500px", maxWidth:"800px", width: "100%", height: "auto" }}>
+      <svg
+        ref={svgRef}
+        viewBox={`0 0 ${width} ${height}`}
+        preserveAspectRatio="xMidYMid meet"
+        style={{ width: "100%", height: "auto" }}
+      />
+    </div>
+  );
+}
