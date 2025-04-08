c0f3035fa8f085cd0cbd264b5f2257ef3d776840
Chukwu Stanley <stanleychukwu17@yahoo.com>
2025-04-08 12:52:09 +0100

    deleted

--- frontend/D3/0-bar-charts/2-animated-barchart/src/main.tsx
+++ frontend/D3/0-bar-charts/2-animated-barchart/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './reset.css'
-import App1 from './components/App/App1'
+import App from './components/App/App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App1 />
+    <App />
   </StrictMode>,
 )
