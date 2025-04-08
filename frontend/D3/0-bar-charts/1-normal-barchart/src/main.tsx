c0c8c69c0d2ef5746f4929348d360f614c8a2ed2
Chukwu Stanley <stanleychukwu17@yahoo.com>
2025-04-08 12:32:57 +0100

    updated the home component

--- frontend/D3/0-bar-charts/1-normal-barchart/src/main.tsx
+++ frontend/D3/0-bar-charts/1-normal-barchart/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './reset.css'
-import App from './App.tsx'
+import App1 from './components/App/App1'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <App1 />
   </StrictMode>,
 )
