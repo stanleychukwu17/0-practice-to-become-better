c5fadfe5ac2252236146655b2320f9145921fe13
Chukwu Stanley <stanleychukwu17@yahoo.com>
2025-04-08 10:59:56 +0100

    initial commit

---
+++ frontend/D3/0-bar-charts/1-normal-barchart/src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+import BarChart from './components/BarChart/BarChart'
+
+const sampleData = [
+    { name: "A", value: 30 },
+    { name: "B", value: 80 },
+    { name: "C", value: 45 },
+    { name: "D", value: 60 },
+    { name: "E", value: 20 }
+];
+
+export default function App1() {
+    return (
+        <div>
+            <h2>Bar Chart with D3 & React</h2>
+            <BarChart data={sampleData} width={500} height={500} />
+        </div>
+    )
+}
