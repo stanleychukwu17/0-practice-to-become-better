a87ae140d8600909b23558dfa82e084e29b43b82
Chukwu Stanley <stanleychukwu17@yahoo.com>
2025-06-08 19:10:12 +0100

    renamed

---
+++ devops/bash/4-updating-system-packages/solution-4.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+apg-get update -y # this will update the package list
+
+apt-get upgrade -y # this will upgrade the packages
+
+apt-get remove -y # this will remove the packages you don't need
