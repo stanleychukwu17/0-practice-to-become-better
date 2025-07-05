743752909904c2c44a81809a391a23655d17186f
Chukwu Stanley <stanleychukwu17@yahoo.com>
2025-07-05 10:31:40 +0100

    we added the content of the header

---
+++ frontend/threejs/1-chairs-with-r3f/src/components/Header/Header.tsx
@@ -0,0 +1,19 @@
+
+export default function Header() {
+  return (
+    <header>
+      <div>
+        <div>STANLEY</div>
+        <nav>
+          <div className=""><a href="#">Discover</a></div>
+          <div className=""><a href="#">Product</a></div>
+          <div className=""><a href="#">Solution</a></div>
+          <div className=""><a href="#">Reach</a></div>
+        </nav>
+        <div className="">
+          <button className="">Order</button>
+        </div>
+      </div>
+    </header>
+  )
+}
