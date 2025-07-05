c1c9d981294f0965c1f2c978e3e7a80c6d51f429
Chukwu Stanley <stanleychukwu17@yahoo.com>
2025-07-05 16:42:14 +0100

    wrote a custom lerp function

--- frontend/threejs/1-chairs-with-r3f/src/components/Section/useSection.ts
+++ frontend/threejs/1-chairs-with-r3f/src/components/Section/useSection.ts
@@ -34,6 +34,10 @@ function useSection() {
   }
 }
 
+function lerp(start: number, end: number, alpha: number): number {
+  return start + (end - start) * alpha;
+}
+
 export {
-  useSection
+  useSection, lerp
 }
