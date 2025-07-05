eea88d15f5b226b03331b43d45713035d48f7bca
Chukwu Stanley <stanleychukwu17@yahoo.com>
2025-07-05 16:39:23 +0100

    started working on the scene, displaying html element in 3d

--- frontend/threejs/1-chairs-with-r3f/src/App.tsx
+++ frontend/threejs/1-chairs-with-r3f/src/App.tsx
@@ -1,4 +1,5 @@
-// import Task0 from "./components/Task0/Task0"
+import { Canvas } from "@react-three/fiber"
+import {Html} from "@react-three/drei"
 import Header from "./components/Header/Header"
 import './App.css'
 
@@ -7,7 +8,14 @@ export default function App() {
   return (
     <div className="App">
       <Header />
-      {/* <Task0 /> */}
+      <Canvas
+        camera={{ position: [0, 0, 120], fov: 70 }}
+      >
+        <Html fullscreen>
+          <div className="title">Hello</div>
+        </Html>
+
+      </Canvas>
     </div>
   )
 }
