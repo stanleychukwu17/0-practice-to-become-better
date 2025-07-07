49ab1d356c47a543fe16f86a1aad22ef8d877450
Chukwu Stanley <stanleychukwu17@yahoo.com>
2025-07-07 10:23:54 +0100

    added the Html component to the Canvas: '

--- frontend/threejs/1-chairs-with-r3f/src/App.tsx
+++ frontend/threejs/1-chairs-with-r3f/src/App.tsx
@@ -1,8 +1,20 @@
 import { Canvas } from "@react-three/fiber"
 import {Html} from "@react-three/drei"
+import Section from "./components/Section/Section"
 import Header from "./components/Header/Header"
 import './App.css'
 
+const HtmlComponent = () => {
+  return(
+    <Section offset={0} factor={1}>
+      <group position={[0, -20, 0]}>
+        <Html fullscreen>
+          <div className="title">Hello</div>
+        </Html>
+      </group>
+    </Section>
+  )
+}
 
 export default function App() {
   return (
@@ -11,10 +23,7 @@ export default function App() {
       <Canvas
         camera={{ position: [0, 0, 120], fov: 70 }}
       >
-        <Html fullscreen>
-          <div className="title">Hello</div>
-        </Html>
-
+        <HtmlComponent />
       </Canvas>
     </div>
   )
