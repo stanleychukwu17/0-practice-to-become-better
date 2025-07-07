0bf9e989fdf2207237fcb347fef5349a0c63da61
Chukwu Stanley <stanleychukwu17@yahoo.com>
2025-07-07 10:51:40 +0100

    added the model comp

--- frontend/threejs/1-chairs-with-r3f/src/App.tsx
+++ frontend/threejs/1-chairs-with-r3f/src/App.tsx
@@ -1,13 +1,22 @@
 import { Canvas } from "@react-three/fiber"
-import {Html} from "@react-three/drei"
+import {Html, useGLTF} from "@react-three/drei"
 import Section from "./components/Section/Section"
 import Header from "./components/Header/Header"
 import './App.css'
+
+const ModelComp = () => {
+  const gltf = useGLTF("/grey.gltf", true)
+  return (
+    <primitive object={gltf.scene} />
+  )
+}
+
 
 const HtmlComponent = () => {
   return(
     <Section offset={0} factor={1}>
       <group position={[0, -20, 0]}>
+        <ModelComp />
         <Html fullscreen>
           <div className="title">Hello</div>
         </Html>
@@ -15,6 +24,7 @@ const HtmlComponent = () => {
     </Section>
   )
 }
+
 
 export default function App() {
   return (
